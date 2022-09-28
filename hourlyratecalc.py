5953da66e4ade84b7e5d3692eea0ae14207cb8ea
lerontonge <lerontonge@gmail.com>
2022-09-27 21:42:28 -0400

    ex_2_3

--- hourlyratecalc.py
+++ hourlyratecalc.py
@@ -5,17 +5,22 @@
 hs = input("Enter Hours: ")
 hr = input("Enter Rate: ")
 
-
-fr = float(hr)
-fh = float(hs)
-
+try:
+    fr = float(hr)
+    fh = float(hs)
+except:
+    print("ERROR ERROR ERROR")
+    quit()
+    
 if fh > 40:
     pay = (40 * fr) + ((fh - 40) * (fr * 0.5))
     print("Overtime")
-
+    
 if fh <= 40:
     pay = fr * fh
     print("Regular")
 
 
 print("Total Pay:", pay)
+
+
